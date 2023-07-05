b024b2e4ca17df387abead457358618eb4d350b0
falguni9 <105406571+falguni9@users.noreply.github.com>
2023-07-05 12:30:22 +0530

    payment file and order table integration

--- React-ecom-backend/routes/product.routes.js
+++ React-ecom-backend/routes/product.routes.js
@@ -1,9 +1,15 @@
 const productController = require('../controllers/productlist.controller');
+const userValidator = require('../middleware/auth.validetor')
 
 module.exports = function(app){
     // to create a product
-    app.post('/reacteccomBack/api/v1/productlist', productController.CreatProductlist);
-     //get all tickets assigned to current user
+    app.post('/reacteccomBack/api/v1/productlist',userValidator.isUserAuthenticated,userValidator.isAdminOrSeller, productController.CreatProductlist);
+     //get all product 
      app.get('/reacteccomBack/api/v1/productlist/all', productController.GetallProductlist);
-
-}
+     //get product by id
+     app.get('/reacteccomBack/api/v1/productlist/:id', productController.GetIDProduct);
+     
+     app.patch('/reacteccomBack/api/v1/productlist/update/:id' ,userValidator.isUserAuthenticated,userValidator.isAdminOrSeller, productController.updateProductlist);
+     
+     app.post('/reacteccomBack/api/v1/productlist/delete/:id' ,userValidator.isUserAuthenticated,userValidator.isAdminOrSeller, productController.deleteProductlist);
+}  
